20da0bc0123a5cc6490e504f20f55a8b1b9c328e
giriritesh123 <139381504+giriritesh123@users.noreply.github.com>
2025-05-01 16:56:11 +0530

    Update astro_api.py

--- astro_api.py
+++ astro_api.py
@@ -1,9 +1,4 @@
-import requests
-
-def get_astrology_report(sign):
-    params = (('sign', sign.lower()), ('day', 'today'))
-    response = requests.post('https://aztro.sameerkumar.website/', params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {"error": "API call failed"}
+curl --request GET \
+	--url 'https://best-daily-astrology-and-horoscope-api.p.rapidapi.com/api/Detailed-Horoscope/?zodiacSign=leo' \
+	--header 'x-rapidapi-host: best-daily-astrology-and-horoscope-api.p.rapidapi.com' \
+	--header 'x-rapidapi-key: b18a9b3721msh531816298cc20a1p17b496jsn143ef513e88d'
