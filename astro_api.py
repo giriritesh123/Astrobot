885106204c929408e455d4eae3f20f966dbfe949
giriritesh123 <139381504+giriritesh123@users.noreply.github.com>
2025-05-01 17:07:58 +0530

    Update astro_api.py

--- astro_api.py
+++ astro_api.py
@@ -3,16 +3,17 @@ import requests
 def get_astrology_report(sign):
     url = "https://best-daily-astrology-and-horoscope-api.p.rapidapi.com/api/Detailed-Horoscope/"
 
-    querystring = {"zodiacSign": sign.lower()}  # e.g., 'leo'
+    querystring = {"zodiacSign": sign}
 
     headers = {
         "x-rapidapi-host": "best-daily-astrology-and-horoscope-api.p.rapidapi.com",
-        "x-rapidapi-key": "b18a9b3721msh531816298cc20a1p17b496jsn143ef513e88d"  # 🔐 your API key
+        "x-rapidapi-key": "b18a9b3721msh531816298cc20a1p17b496jsn143ef513e88d"  # 🔐 Your key
     }
 
     response = requests.get(url, headers=headers, params=querystring)
 
-    if response.status_code == 200:
+    try:
         return response.json()
-    else:
-        return {"error": "API call failed"}
+    except:
+        return {"error": "Failed to decode JSON"}
+
